8811bca6ad0927807957561e9bfc80bf1c5c448b
Muhammad Mumtaz <mumtazbaly@gmail.com>
2026-04-19 04:06:31 +0500

    .

--- static-website/06-outputs.tf
+++ static-website/06-outputs.tf
@@ -1,3 +1,4 @@
-output "name" {
-  value = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
+output "webendpoint" {
+  description = "static website end point hosted in s3"
+  value       = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
 }
