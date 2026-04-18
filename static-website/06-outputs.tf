09d52b9fea1906fb63263dfc1a978932ce98cb7a
Muhammad Mumtaz <mumtazbaly@gmail.com>
2026-04-19 04:10:39 +0500

    feat: add description for  end point url for the static hosted web page in s3

--- static-website/06-outputs.tf
+++ static-website/06-outputs.tf
@@ -1,3 +1,4 @@
-output "name" {
-  value = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
+output "staticwebendpoint" {
+  description = "end point url for the static hosted web page in s3"
+  value       = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
 }
