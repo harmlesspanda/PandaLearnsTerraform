edf371c466c0bba61d8a77fba74601c69e918bdd
Muhammad Mumtaz <mumtazbaly@gmail.com>
2026-04-19 03:54:30 +0500

    feat: use s3 for hosting static website

---
+++ static-website/02-providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
